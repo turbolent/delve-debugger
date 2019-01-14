813680be895694c6d695cace169ffb8529c470b7
Bastian Müller <bastian@turbolent.com>
2019-01-13 19:03:43 -0800

    electron app: support multiple windows and pass hash of opened URL

--- electron/main.ts
+++ electron/main.ts
@@ -4,25 +4,26 @@ import * as isDev from "electron-is-dev";
 
 const protocol = "question-debugger";
 
-let mainWindow: BrowserWindow | undefined;
+const windows = new Set<BrowserWindow>();
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
+function createWindow(hash?: string) {
+  const window = new BrowserWindow({
     webPreferences: {
       nodeIntegration: false
     }
   });
-  if (!mainWindow) {
+  if (!window) {
     return;
   }
-  mainWindow.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "index.html")}`
-  );
-  mainWindow.on("closed", () => {
-    mainWindow = undefined;
-  });
+  windows.add(window);
+
+  const baseURL = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "index.html")}`;
+
+  const url = baseURL + (hash || "");
+  window.loadURL(url);
+  window.on("closed", () => windows.delete(window));
 }
 
 app.on("ready", () => {
@@ -44,10 +45,12 @@ app.on("open-url", (event, rawURL) => {
   if (parsedURL.protocol !== protocol + ":") {
     return;
   }
+
+  createWindow(parsedURL.hash);
 });
 
 app.on("activate", () => {
-  if (!mainWindow) {
+  if (!windows.size) {
     createWindow();
   }
 });
