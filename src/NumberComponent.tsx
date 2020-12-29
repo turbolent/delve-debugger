02f04ff1ea387dfc0b29a639f729e135f07ed09a
Bastian Müller <bastian@turbolent.com>
2020-12-29 15:17:22 -0800

    please linter

--- src/NumberComponent.tsx
+++ src/NumberComponent.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
+import {ReactElement} from "react";
 
 interface Props {
   unit?: { name: string };
   value: number;
 }
 
-export default ({ value, unit }: Props) => {
+function NumberComponent({value, unit}: Props): ReactElement {
   const suffix = unit ? `(${unit.name})` : "";
   return (
     <div>
       {value} {suffix}
     </div>
   );
-};
+}
+
+export default NumberComponent;
